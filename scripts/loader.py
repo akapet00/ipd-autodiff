e9b7fc609c800ed6de67fc43526742221657da23
antelk <alojic00@fesb.hr>
2021-05-13 12:22:16 +0200

    small updates

--- scripts/loader.py
+++ scripts/loader.py
@@ -4,7 +4,7 @@ import pandas as pd
 from scipy.io import loadmat
 
 
-SUPPORTED_FREQS = [3., 6., 10., 15., 20., 30., 40., 60., 80., 100.]
+SUPPORTED_FREQS = [3., 3.5, 6., 10., 15., 20., 30., 40., 60., 80., 100.]
 
 
 def load_antenna_el_properties(frequency):
@@ -29,9 +29,9 @@ def load_antenna_el_properties(frequency):
     assert frequency / 1e9 in SUPPORTED_FREQS, \
         (f'{frequency / 1e9} is not in supported. '
          f'Supported frequency values: {SUPPORTED_FREQS}.')
-    data = loadmat(os.path.join('data', 'current.mat'))['output']
-    df = pd.DataFrame(data,
-                      columns=['L', 'N', 'r', 'f', 'x', 'ireal', 'iimag'])
+    data_dir = loadmat(os.path.join('data', 'fs_current', 'dataset.mat'))
+    df = pd.DataFrame(data_dir['output'],
+                      columns=['N', 'f', 'L', 'V', 'x', 'ireal', 'iimag'])
     df_f = df[df.f == frequency]
     df_f.reset_index(drop=True, inplace=True)
     return df_f
